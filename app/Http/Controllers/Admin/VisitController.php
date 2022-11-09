1ebc7082a57607845034af2ec99d9622d1f88d21
Arfaniel <dn120284aio@gmail.com>
2022-11-09 15:06:56 +0200

    20-th homework, Queue not working :(

--- app/Http/Controllers/Admin/VisitController.php
+++ app/Http/Controllers/Admin/VisitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\VisitPersisting;
 use App\Models\Visit;
 use App\Services\UserAgent\UserAgentServiceInterface;
 use Vagrant\Geo\PackageGeoInterface\GeoServiceInterface;
@@ -15,15 +16,18 @@ class VisitController extends Controller
         if ($ip == '127.0.0.1') {
             $ip = request()->server->get('HTTP_X_FORWARDED_FOR');
         }
-        $userAgentReader->parse($_SERVER['HTTP_USER_AGENT']);
-        $country_code = $geoReader->getCountry() ?? 'UN';
-        $continent_code = $geoReader->getIsoCode() ?? 'UN';
-        Visit::create([
-            'ip' => $ip,
-            'country_code' => $country_code,
-            'continent_code' => $continent_code,
-            'browser_name' => $userAgentReader->getBrowser(),
-            'os_name' => $userAgentReader->getOs()
-        ]);
+        VisitPersisting::dispatch($ip, $geoReader, $userAgentReader)->onQueue('parsing');
+
+//        $geoReader->parse($ip);
+//        $userAgentReader->parse($_SERVER['HTTP_USER_AGENT']);
+//        $country_code = $geoReader->getCountry() ?? 'UN';
+//        $continent_code = $geoReader->getIsoCode() ?? 'UN';
+//        Visit::create([
+//            'ip' => $ip,
+//            'country_code' => $country_code,
+//            'continent_code' => $continent_code,
+//            'browser_name' => $userAgentReader->getBrowser(),
+//            'os_name' => $userAgentReader->getOs()
+//        ]);
     }
 }
---
+++ app/Jobs/VisitPersisting.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Visit;
+use App\Services\UserAgent\UserAgentServiceInterface;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Vagrant\Geo\PackageGeoInterface\GeoServiceInterface;
+
+class VisitPersisting implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $ip;
+    public $country_code;
+    public $continent_code;
+    public $browser_name;
+    public $os_name;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(string $ip, GeoServiceInterface $geoReader, UserAgentServiceInterface $userAgentReader)
+    {
+        $geoReader->parse($ip);
+        $userAgentReader->parse($_SERVER['HTTP_USER_AGENT']);
+        $this->country_code = $geoReader->getCountry() ?? 'UN';
+        $this->continent_code = $geoReader->getIsoCode() ?? 'UN';
+        $this->browser_name = $userAgentReader->getBrowser();
+        $this->os_name = $userAgentReader->getOs();
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Visit::create([
+            'ip' => $this->ip,
+            'country_code' => $this->country_code,
+            'continent_code' => $this->continent_code,
+            'browser_name' => $this->browser_name,
+            'os_name' => $this->os_name
+        ]);
+    }
+}
