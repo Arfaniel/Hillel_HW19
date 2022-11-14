f1a72727b9d4a43317c8ecf76c67188eefeba7e2
Arfaniel <dn120284aio@gmail.com>
2022-11-14 19:22:27 +0200

    20-th homework, correct seeds and factory is added

--- app/Http/Controllers/Admin/VisitController.php
+++ app/Http/Controllers/Admin/VisitController.php
@@ -6,22 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Jobs\VisitPersisting;
 use App\Models\Visit;
 use App\Services\UserAgent\UserAgentServiceInterface;
+use Faker\Factory;
 use Vagrant\Geo\PackageGeoInterface\GeoServiceInterface;
 
 class VisitController extends Controller
 {
     public function index(GeoServiceInterface $geoReader, UserAgentServiceInterface $userAgentReader)
     {
-        $ip = request()->ip();
+        $faker = Factory::create();
+        $ip = $faker->ipv4();
         if ($ip == '127.0.0.1') {
             $ip = request()->server->get('HTTP_X_FORWARDED_FOR');
         }
         $geoReader->parse($ip);
-        $userAgentReader->parse($_SERVER['HTTP_USER_AGENT']);
+        $userAgentReader->parse($faker->userAgent());
         $country_code = $geoReader->getCountry() ?? 'UN';
         $continent_code = $geoReader->getIsoCode() ?? 'UN';
         $browser_name = $userAgentReader->getBrowser();
         $os_name = $userAgentReader->getOs();
+
         VisitPersisting::dispatch($ip, $country_code, $continent_code, $browser_name, $os_name)->onQueue('parsing');
     }
 }
---
+++ database/factories/VisitFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Visit;
+use App\Services\UserAgent\UserAgentServiceInterface;
+use App\Services\UserAgent\WhichBrowserService;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Vagrant\Geo\PackageGeoInterface\GeoServiceInterface;
+use Vagrant\PackageGeoIpApiGeoService\IpApiGeoService;
+use Vagrant\PackageGeoMaxmindservice\MaxmindService;
+use WhichBrowser\Analyser\Header\Useragent;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Visit>
+ */
+class VisitFactory extends Factory
+{
+    protected $model = Visit::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $geoReader = new MaxmindService();
+        $userAgentReader = new WhichBrowserService();
+        $userAgentReader->parse(fake()->userAgent());
+        $ip = fake()->ipv4;
+        $geoReader->parse($ip);
+        print_r($geoReader->getIsoCode());
+        return [
+            'ip'=> $ip,
+            'country_code'=> $geoReader->getCountry() ?? 'UN',
+            'continent_code'=> $geoReader->getIsoCode() ?? 'UN',
+            'browser_name' => $userAgentReader->getBrowser(),
+            'os_name' => $userAgentReader->getOs()
+        ];
+    }
+}
