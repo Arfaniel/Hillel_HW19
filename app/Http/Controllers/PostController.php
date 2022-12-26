d371d93e11a94c7e4f4aa64bdbcfc55393c4a435
Arfaniel <dn120284aio@gmail.com>
2022-12-26 18:19:18 +0200

    Trying to use dropbox for filestorage

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
 
 class PostController
 {
     public function index()
     {
-        $posts = Post::with('category', 'user', 'tags')->get();
-        return view('index', compact('posts'));
+        Storage::disk('dropbox')->put('example.txt', 'content');
+//        $posts = Post::with('category', 'user', 'tags')->get();
+//        return view('index', compact('posts'));
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
+
 use Illuminate\Contracts\Http\Kernel;
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
